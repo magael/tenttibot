0adc9ca8105bf9178b79c03225e23da3f4d16d92
Mikael Jaakkola <mikael.jaakkola@helsinki.fi>
2018-08-07 00:19:26 +0300

    actual login

--- application/__init__.py
+++ application/__init__.py
@@ -1,12 +1,15 @@
+# flask app
 from flask import Flask
 app = Flask(__name__)
 
+# database
 from flask_sqlalchemy import SQLAlchemy
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///questions.db"
 app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
+# functionalities of this app
 from application import views
 
 from application.questions import models
@@ -15,4 +18,21 @@ from application.questions import views
 from application.auth import models
 from application.auth import views
 
+# logging in
+from application.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Please login to use this functionality."
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+# create tables when necessary
 db.create_all()
