23c80bacbe402adc125055f0bd32f1604b3ba2e8
Mikael Jaakkola <mikael.jaakkola@helsinki.fi>
2018-08-12 19:52:32 +0300

    listing questions by subject

--- application/questions/models.py
+++ application/questions/models.py
@@ -1,8 +1,11 @@
 from application import db
 from application.models import Base
 
-# extends class Base (in application/models)
+from sqlalchemy.sql import text
+
+
 class Question(Base):
+    """extends class Base (in application/models)"""
     mastered = db.Column(db.Boolean, nullable=False)
 
     subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'),
@@ -11,3 +14,17 @@ class Question(Base):
     def __init__(self, name):
         self.name = name
         self.mastered = False
+
+    @staticmethod
+    def find_questions_by_subject(subject_id):
+        # TODO:
+        stmt = text("SELECT Question.id, Question.name, Question.mastered FROM Question"
+                    " LEFT JOIN Subject on Subject.id = Question.subject_id"
+                    " GROUP BY Question.id")
+        res = db.engine.execute(stmt)
+
+        response = []
+        for row in res:
+            response.append({"id":row[0], "name":row[1], "mastered":row[2]})
+
+        return response
--- application/questions/views.py
+++ application/questions/views.py
@@ -5,79 +5,86 @@ from application import app, db
 from application.questions.models import Question
 from application.questions.forms import QuestionForm
 
-# list questions page
 @app.route("/questions", methods=["GET"])
 def questions_index():
-    return render_template("questions/list.html", questions = Question.query.all())
+    """Page for isting questions."""
+    # TODO: query only where "subject = subject":
+    # trying to get subject_id "from url"
+    s = request.args.get('subject_id')
+    return render_template("questions/list.html", questions = Question.find_questions_by_subject(s))
+    # return render_template("questions/list.html", questions=Question.query.all())
 
-# new question creation page
 @app.route("/questions/new/")
 @login_required
 def questions_form():
-    return render_template("questions/new.html", form = QuestionForm())
+    """Page for creating a new question"""
+    return render_template("questions/new.html", form=QuestionForm())
 
-# question editing page
 @app.route("/questions/<question_id>/", methods=["GET"])
 @login_required
 def questions_question(question_id):
+    """Page for question editing"""
     form = QuestionForm(request.form)
     q = Question.query.get(question_id)
-    return render_template("questions/question.html", form = form, question = q)
+    return render_template("questions/question.html", form=form, question=q)
 
 # # updating a question's status
-# @app.route("/questions/<question_id>/", methods=["POST"]) # better route ...id>/mastered ?
+# @app.route("/questions/<question_id>/", methods=["POST"]) # IDEA: better route ...id>/mastered ?
 # @login_required
 # def questions_set_mastered(question_id):
 #     q = Question.query.get(question_id)
 #     q.mastered = not q.mastered
 #     db.session().commit()
-  
+
 #     return redirect(url_for("questions_index"))
 
-# posting data to edit a question
 @app.route("/questions/<question_id>/edit/", methods=["POST"])
 @login_required
 def questions_edit(question_id):
+    """Posting data to edit a question"""
     form = QuestionForm(request.form)
     q = Question.query.get(question_id)
 
     if not form.validate():
-        return render_template("questions/new.html", form = form)
+        return render_template("questions/new.html", form=form)
 
     q.name = form.name.data
     q.mastered = form.mastered.data
 
-    db.session().commit()
-  
-    return redirect(url_for("questions_index"))
+    s = q.subject_id
 
-# deleting a question
+    db.session().commit()
+
+    return redirect(url_for("questions_index", subject_id=s))
+
 @app.route("/questions/<question_id>/delete/", methods=["POST"])
 @login_required
 def questions_delete(question_id):
+    """Deleting a question"""
     q = Question.query.get(question_id)
+    s = q.subject_id
     db.session().delete(q)
     db.session().commit()
 
-    return redirect(url_for("questions_index"))
+    return redirect(url_for("questions_index", subject_id=s))
 
-# post data to create a new question
 @app.route("/questions/", methods=["POST"])
 @login_required
 def questions_create():
+    """Post data to create a new question"""
     form = QuestionForm(request.form)
 
     if not form.validate():
-        return render_template("questions/new.html", form = form)
+        return render_template("questions/new.html", form=form)
 
     q = Question(form.name.data)
     q.mastered = form.mastered.data
-    # TEMP!
-    q.subject_id = 1 # CHANGE when subject more developed
-    # !
+    # HACK
+    q.subject_id = 1  # TODO: get actual current subject id when subject more developed
+    
+    s = q.subject_id
 
     db.session().add(q)
     db.session().commit()
-  
-    return redirect(url_for("questions_index"))
-    
+
+    return redirect(url_for("questions_index", subject_id=s))
