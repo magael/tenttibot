c5085ebbc8fb071bd4fcce1dac53550bd552014b
Mikael Jaakkola <mikael.jaakkola@helsinki.fi>
2018-08-15 00:41:23 +0300

    bootstrap käyttöön, pientä käyttöliittymän viilausta

--- application/subjects/models.py
+++ application/subjects/models.py
@@ -1,8 +1,9 @@
 from application import db
 from application.models import Base
 
-# extends class Base in (application/models)
+
 class Subject(Base):
+    """extends class Base (in application/models)"""
     questions = db.relationship("Question", backref='subject', lazy=True)
 
     def __init__(self, name):
