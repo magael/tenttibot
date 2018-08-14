b106950b5a91ca8858386bae8b3a2a3d63217ec8
Mikael Jaakkola <mikael.jaakkola@helsinki.fi>
2018-08-14 15:18:21 +0300

    rekisteröintilomake perii kirjautumislomakkeen

--- application/auth/forms.py
+++ application/auth/forms.py
@@ -2,20 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, validators
 
 
-# TODO: Base class AuthForm?
-
-
 class LoginForm(FlaskForm):
-    # ok not to validate since it's done in registration, right?
-    username = StringField("Username")
-    password = PasswordField("Password")
-
-    class Meta:
-        csrf = False
-
-
-class RegistrationForm(FlaskForm):
-    name = StringField("Name", [validators.Length(min=2, max=144)])
     username = StringField("Username", [
         validators.Length(min=2, max=144),
         validators.Regexp('^\w+$',
@@ -25,3 +12,8 @@ class RegistrationForm(FlaskForm):
 
     class Meta:
         csrf = False
+
+
+class RegistrationForm(LoginForm):
+    """extends LoginForm"""
+    name = StringField("Name", [validators.Length(min=2, max=144)])
