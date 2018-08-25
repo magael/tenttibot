c376d300514ac228c8acc93a5267f6cfb9ae83a5
Mikael Jaakkola <mikael.jaakkola@helsinki.fi>
2018-08-25 18:19:49 +0300

    oman aihealueen muokkaus ja poisto (mukaanlukien kaikki sen kysymykset), users-nappi

--- application/subjects/views.py
+++ application/subjects/views.py
@@ -1,21 +1,23 @@
 from flask import redirect, render_template, request, url_for
 from flask_login import login_required, current_user
 
-from application import app, db
+from application import app, db, login_manager, login_required
 from application.auth.models import user_subjects
-from application.subjects.models import Subject
 from application.subjects.forms import SubjectForm
+from application.subjects.models import Subject
+from application.questions.models import Question
+from application.questions.views import is_creator
 
 
 @app.route("/new/", methods=["GET"])
-@login_required
+@login_required()
 def subjects_form():
     """Page for creating a new subject"""
     return render_template("subjects/new.html", form=SubjectForm())
 
 
 @app.route("/", methods=["POST"])
-@login_required
+@login_required()
 def subjects_create():
     """Post data to create a new subject"""
     form = SubjectForm(request.form)
@@ -29,5 +31,49 @@ def subjects_create():
     
     db.session.add(s)
     db.session.commit()
+
+    return redirect(url_for("index"))
+
+@app.route("/<subject_id>/edit/", methods=["GET"])
+@login_required()
+def subjects_subject(subject_id):
+    """Page for subject editing"""
+    form = SubjectForm(request.form)
+    s = Subject.query.get(subject_id)
+    return render_template("subjects/subject.html", form=form, subject=s)
+
+@app.route("/<subject_id>/edit/", methods=["POST"])
+@login_required()
+def subjects_edit(subject_id):
+    """Posting data to edit a subject"""
+    if not is_creator(subject_id):
+        return login_manager.unauthorized()
+
+    form = SubjectForm(request.form)
+    s = Subject.query.get(subject_id)
+
+    if not form.validate():
+        return render_template("subjects/subject.html", form=form, subject=s)
+
+    s.name = form.name.data
+
+    db.session().commit()
+
+    return redirect(url_for("questions_index", subject_id=subject_id))
+
+@app.route("/<subject_id>/delete/", methods=["POST"])
+@login_required()
+def subjects_delete(subject_id):
+    """Deleting a subject and all it's questions"""
+    if not is_creator(subject_id):
+        return login_manager.unauthorized()
+
+    s = Subject.query.get(subject_id)
+    questions = Question.query.filter_by(subject_id=subject_id)
+
+    for q in questions:
+        db.session().delete(q)
+    db.session().delete(s)
+    db.session().commit()
 
     return redirect(url_for("index"))
