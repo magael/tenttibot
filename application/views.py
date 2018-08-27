94553f55ce9f4d44dabf9d4942703ef65309acc9
Mikael Jaakkola <mikael.jaakkola@helsinki.fi>
2018-08-28 00:16:36 +0300

    testailun turhia importteja pois

--- application/views.py
+++ application/views.py
@@ -2,16 +2,8 @@ from flask import render_template
 from flask_login import current_user
 
 from application import app
-from application.subjects.models import Subject
-
-from flask import redirect, render_template, request, url_for
-from flask_login import current_user
-
-from application import app, db, login_manager, login_required
 from application.auth.models import User
 from application.subjects.models import Subject
-from application.questions.models import Question
-from application.questions.forms import QuestionForm
 
 @app.route("/")
 def index():
