b09b5188c6b92c6c5d3b2b60ae3a11022d4e4939
Mikael Jaakkola <mikael.jaakkola@helsinki.fi>
2018-08-30 02:19:47 +0300

    kysymyslomakkeen validointiviesteihin lisäys, paddingia korttiin

--- application/questions/forms.py
+++ application/questions/forms.py
@@ -2,8 +2,8 @@ from flask_wtf import FlaskForm
 from wtforms import BooleanField, StringField, IntegerField, validators
 
 class QuestionForm(FlaskForm):
-    name = StringField("Question", [validators.Length(min=1, max=144)])
-    answer = StringField("Answer", [validators.Length(min=1, max=144)])
+    name = StringField("Question", [validators.Length(min=1, max=144, message = 'Field must be between 1 and 144 characters long. Go backwards one page with your browser to restore the content.')])
+    answer = StringField("Answer", [validators.Length(min=1, max=144, message = 'Field must be between 1 and 144 characters long. Go backwards one page with your browser to restore the content.')])
     mastery = IntegerField("Mastery", [validators.NumberRange(min=0, max=5)])
  
     class Meta:
