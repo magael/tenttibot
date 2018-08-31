f2ee7a2305c7fbbff5d6c327816a718a3ea7330c
Mikael Jaakkola <mikael.jaakkola@helsinki.fi>
2018-08-31 22:20:37 +0300

    uuden kysymyksen luonnin (masteryn alkuarvon validointiin liittyvän) bugin korjaus

--- application/questions/forms.py
+++ application/questions/forms.py
@@ -4,7 +4,7 @@ from wtforms import BooleanField, StringField, IntegerField, validators
 class QuestionForm(FlaskForm):
     name = StringField("Question", [validators.Length(min=1, max=144)])
     answer = StringField("Answer", [validators.Length(min=1, max=144)])
-    mastery = IntegerField("Mastery", [validators.NumberRange(min=0, max=5)])
+    mastery = IntegerField("Mastery", [validators.NumberRange(min=0, max=5)], default=0)
  
     class Meta:
         csrf = False
